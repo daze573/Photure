910ff2c2fd61920c3b888840efa140a22b2b9fab
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-05 10:01:50 +0000

    [Add] 退会画面

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,8 +4,26 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def resign
+    user = current_user
+    user.update(user_status: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image)
   end
 end
