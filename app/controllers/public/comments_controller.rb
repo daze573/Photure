1a72b66999de7545d5a7d495c325451d2c5ce246
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-06 09:51:20 +0000

    [Update]　コメント削除機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -10,6 +10,10 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
   private
