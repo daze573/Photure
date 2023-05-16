412e4213badf981a6db7c6a1a62cc2d76c4cba3b
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-16 05:30:09 +0000

    [Update] 本番環境での初期データ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: "admin@admin",
-  password: "aaaaaa"
-)
-User.create!(
-  name: "山田",
-  image: File.open("./app/assets/images/no_image.jpg"),
-  email: "test@test.com",
-  password: "aaaaaa",
-  introduction: "よろしくお願いします！",
-  user_status: false
+  email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD']
 )
 Genre.create!(
-  name: "写真"
-)
-Post.create!(
-  user_id: 1,
-  genre_id: 1,
-  title: "デスクトップ",
-  introduction: "デスクトップのイメージ写真",
-  image: File.open("./app/assets/images/desk.jpg"),
-  status: 1
-)
-Favorite.create!(
-  user_id: 1,
-  post_id: 1
-)
-Comment.create!(
-  user_id: 1,
-  post_id: 1,
-  comment: "ありがとうございます"
-)
-Tag.create!(
-  name: "エンジニア"
-)
-PostTag.create!(
-  post_id: 1,
-  tag_id: 1
+  [
+    {
+      name: "写真"
+    },
+    {
+      name: "イラスト"
+    }
+  ]
 )
