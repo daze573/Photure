bc3fac1fe264e56b984360fa88bde6a51b7019aa
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-17 04:53:55 +0000

    [Fix] モーダル設定の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_post
+
+  private
+
+  def set_post
+    @post = Post.find_by(params[:id])
+    redirect_to root_path, notice: "Post not found" unless @post
+  end
 end
