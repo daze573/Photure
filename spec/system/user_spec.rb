dfeae5e9234dc0c325de937344f78ad209a79a62
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-21 14:33:26 +0000

    [Add] rspecテスト　system/user_spec.rb

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -20,4 +20,17 @@ describe "会員の新規登録テスト" do
       expect(page).to have_button('commit')
     end
   end
+  context "新規登録テスト" do
+    before do
+      visit new_user_registration_path
+    end
+    it "名前を入力する" do
+      fill_in 'user[name]', with: Faker::Lorem.characters(number:10)
+      fill_in 'user[email]', with: "example@example.com"
+      fill_in 'user[password]', with: "aaaaaa"
+      fill_in 'user[password_confirmation]', with: "aaaaaa"
+      click_button "登録"
+      expect(current_path).to eq root_path
+    end
+  end
 end
