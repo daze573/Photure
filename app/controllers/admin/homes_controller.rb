cbbd420158768bdc0fd9f3ea6929130b2d93c833
daze573 <124431662+daze573@users.noreply.github.com>
2023-05-27 12:41:28 +0000

    [Fix] 管理者側の作品一覧の新着順を修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,7 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     @user = current_user
-    @posts = Post.all.page(params[:page]).per(12)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(12)
     @genres = Genre.all
   end
 end
