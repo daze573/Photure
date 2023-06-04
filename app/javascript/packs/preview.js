71bf00ae5f067569f57b99b479d7f0afeab9a8b3
daze573 <124431662+daze573@users.noreply.github.com>
2023-06-04 10:19:54 +0000

    [Update] 複数画像の投稿をプレビュー化

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,9 +1,24 @@
 $(document).on('turbolinks:load', function() {
   $('.img_field').on('change', function (e) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        $(".img_prev").attr('src', e.target.result);
+    if (e.target.files.length > 4) {
+      alert('一度に投稿できるのは五枚までです。');
+      $('.img_field').val("");
+      for (var i = 0; i < 4; i++) {
+        $('#preview_' + i).attr('src', "");
+      }
+    } else {
+      for (var i = 0; i < 4; i++) {
+        $('#preview_' + i).attr('src', "");
+      }
+      for (var i = 0; i < e.target.files.length && i < 4; i++) {
+        var reader = new FileReader();
+        reader.onload = (function (index) {
+          return function (e) {
+            $('#preview_' + index).attr('src', e.target.result);
+          };
+        })(i);
+        reader.readAsDataURL(e.target.files[i]);
+      }
     }
-    reader.readAsDataURL(e.target.files[0]);
   });
 });
