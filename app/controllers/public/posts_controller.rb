19153bc2ff484b5ce8033e6807488b00c921dd93
daze573 <124431662+daze573@users.noreply.github.com>
2023-06-07 09:46:52 +0000

    [Fix]更新されない不具合の修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -35,19 +35,12 @@ class Public::PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
     @tag_list = @post.tags.pluck(:name).join('　')
-    @post.post_images.build if @post.post_images.blank? # 空の場合に新しい画像フィールドを追加
+    (4 - @post.post_images.count).times { @post.post_images.build }
   end
 
   def update
     @post = Post.find(params[:id])
     tag_list = params[:post][:tag].split('　')
-
-    # 画像の編集処理
-    if params[:post][:post_images_images].present?
-      Array.wrap(params[:post][:post_images_images]).each do |image|
-        @post.post_images.create(image: image)
-      end
-    end
 
     if @post.update(post_params)
       @post.save_tag(tag_list)
@@ -89,7 +82,7 @@ class Public::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :introduction, :genre_id, post_images_attributes: [:id, :image])
+    params.require(:post).permit(:title, :introduction, :genre_id, post_images_attributes: [:image, :id])
   end
 
   def authenticate!
