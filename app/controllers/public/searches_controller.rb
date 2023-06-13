d56bf5c15f8d2d8fa2fe2f16e64e8bc641c827e5
daze573 <124431662+daze573@users.noreply.github.com>
2023-06-13 05:29:34 +0000

    [Fix] 良いね一覧をエラー修正　検索ワードの表示修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,6 +2,7 @@ class Public::SearchesController < ApplicationController
   def search
     if params[:title].present?
       @posts = Post.where('title LIKE ?', "%#{params[:title]}%").page(params[:page]).per(12)
+      @title = params[:title]
     else
       @posts = Post.none
     end
